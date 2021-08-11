4666670e8b8c3281e0f5f1d25d06650b64f7edad
erikablanco <84459860+erikablanco@users.noreply.github.com>
2021-08-11 10:46:11 -0500

    informacion de la api solo por console.log

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import { GetData } from "./utils/getData";
+import {InfoApi} from "./utils/InfoApi"
 
 function App() {
   GetData();
+  InfoApi();
   return (
     <div className="App">
       hola
